8a9c89d7ecce36300adbf272a88426928dc77837
mwakulinski <michal.wakulinski1@gmail.com>
2022-06-22 11:42:39 +0200

    add getAllPeople()

--- src/starwars/starwars.controller.ts
+++ src/starwars/starwars.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { StarwarsService } from './starwars.service';
 
 @Controller('starwars')
-export class StarwarsController {}
+export class StarwarsController {
+  constructor(private readonly starwatsService: StarwarsService) {}
+  @Get()
+  async getAllPeople() {
+    return this.starwatsService.getAllPeople();
+  }
+}
--- src/starwars/starwars.service.ts
+++ src/starwars/starwars.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class StarwarsService {}
+export class StarwarsService {
+  async getAllPeople() {
+    try {
+      const { data } = await axios.get('https://swapi.dev/api/people');
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
