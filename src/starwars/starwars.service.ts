1b6404dc3795c87572a04270505e85ba775329b8
mwakulinski <michal.wakulinski1@gmail.com>
2022-06-22 12:47:45 +0200

    update: getAllPeople to use getPageOfData

--- src/starwars/starwars.service.ts
+++ src/starwars/starwars.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import { IUserTypeAll } from '../interfaces/interfaces';
+import { ICharacter, IUserTypeAll } from '../interfaces/interfaces';
 
 @Injectable()
 export class StarwarsService {
-  async getAllPeople() {
+  async getAllPeople(
+    page: number = 1,
+    charactersArray: ICharacter[] = [],
+  ): Promise<ICharacter[]> {
+    const data = await this.getPageOfData('people', page);
+    charactersArray.push(...data.results);
+    if (data.next !== null) {
+      page += 1;
+      return this.getAllPeople(page, charactersArray);
+    }
+    return charactersArray;
+  }
+
+  private async getPageOfData(
+    urlParam: string,
+    page: number,
+  ): Promise<IUserTypeAll> {
     try {
       const { data }: { data: IUserTypeAll } = await axios.get(
-        'https://swapi.dev/api/people',
+        `https://swapi.dev/api/${urlParam}`,
+        { params: { page: page } },
       );
       return data;
     } catch (error) {
