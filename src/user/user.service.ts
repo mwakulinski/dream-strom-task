9e36dfcc088f0d7b1b151b39f97c2b6758d94e55
mwakulinski <michal.wakulinski1@gmail.com>
2022-06-22 22:12:29 +0200

    add findUserByEmail()

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/createUser.dto';
+import { User } from './entity/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  private async findUserByEmail(email: string): Promise<User> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+}
