0261fcf22ff972902f55fa4a7a621973ed0343cb
mwakulinski <michal.wakulinski1@gmail.com>
2022-06-23 21:10:20 +0200

    add comparePasswords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,4 +6,8 @@ export class AuthService {
   async hashPassword(password: string) {
     return bcrypt.hash(password, 10);
   }
+
+  async comparePasswords(providedPassword: string, storedPassword: string) {
+    return bcrypt.compare(providedPassword, storedPassword);
+  }
 }
