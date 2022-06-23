b2c15cc3d1c1801fd31802fd51851916d9807901
mwakulinski <michal.wakulinski1@gmail.com>
2022-06-23 22:49:31 +0200

    add generateJwt()

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { IUser } from '../interfaces/interfaces';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async generateJwt(user: IUser) {
+    return this.jwtService.signAsync({ user });
+  }
+
   async hashPassword(password: string) {
     return bcrypt.hash(password, 10);
   }
