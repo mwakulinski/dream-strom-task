2e5f162fdb2b1027b447c582905cdfe0e30863fb
mwakulinski <michal.wakulinski1@gmail.com>
2022-06-23 14:05:08 +0200

    add filterWeatherData()

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import { IWeatherResponse } from 'src/interfaces/interfaces';
+import { IWeather, IWeatherResponse } from 'src/interfaces/interfaces';
 
 @Injectable()
 export class WeatherService {
@@ -11,6 +11,24 @@ export class WeatherService {
         params: { q, APPID },
       },
     );
-    return data;
+    return this.filterWeatherData(data);
+  }
+
+  private filterWeatherData(dataToFilter: IWeatherResponse) {
+    return {
+      name: dataToFilter.name,
+      temp: dataToFilter.main.temp,
+      feels_like: dataToFilter.main.temp,
+      temp_min: dataToFilter.main.temp_min,
+      temp_max: dataToFilter.main.temp_max,
+      pressure: dataToFilter.main.pressure,
+      humidity: dataToFilter.main.humidity,
+      sunrise: dataToFilter.sys.sunrise,
+      sunset: dataToFilter.sys.sunset,
+      visibility: dataToFilter.visibility,
+      windSpeed: dataToFilter.wind.speed,
+      windDeg: dataToFilter.wind.deg,
+      description: dataToFilter.weather[0].description,
+    } as IWeather;
   }
 }
