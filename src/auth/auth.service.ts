d4ab05e5104fd11fbeba2a92caa9ec5f7e537d1a
mwakulinski <michal.wakulinski1@gmail.com>
2022-06-23 21:07:38 +0200

    add hashPassword

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
+  }
+}
