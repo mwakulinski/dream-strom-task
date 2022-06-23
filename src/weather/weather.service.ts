63721ee4e27e64557a5f3cebfa66b3ddffa11f73
mwakulinski <michal.wakulinski1@gmail.com>
2022-06-23 13:49:03 +0200

    add getWeather()

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { IWeatherResponse } from 'src/interfaces/interfaces';
 
 @Injectable()
-export class WeatherService {}
+export class WeatherService {
+  async getWeather(q: string, APPID: string) {
+    const { data }: { data: IWeatherResponse } = await axios.get(
+      'https://api.openweathermap.org/data/2.5/weather',
+      {
+        params: { q, APPID },
+      },
+    );
+    return data;
+  }
+}
